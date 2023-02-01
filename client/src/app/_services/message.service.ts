558ad0e041d2067f4c5153b65b7c36fca7b4e0db
Andrew Zakutko <zakutkoandrey@gmail.com>
2023-02-01 17:49:19 +0200

    End of section 19

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -8,6 +8,7 @@ import { environment } from 'src/environments/environment';
 import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
+import { BusyService } from './busy.service';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 @Injectable({
@@ -20,13 +21,14 @@ export class MessageService implements OnInit {
   private messageThreadSource = new BehaviorSubject<Message[]>([]);
   messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private busyService: BusyService) { }
 
   ngOnInit(): void {
     
   }
 
   createHubConnection(user: User, otherUsername: string){
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
         accessTokenFactory: () => user.token
@@ -34,7 +36,9 @@ export class MessageService implements OnInit {
       .withAutomaticReconnect()
       .build();
 
-    this.hubConnection.start().catch(error => console.log(error));
+    this.hubConnection.start()
+      .catch(error => console.log(error))
+      .finally(() => this.busyService.idle());
 
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
@@ -69,6 +73,7 @@ export class MessageService implements OnInit {
   stopHubConnection() {
     if (this.hubConnection)
     {
+      this.messageThreadSource.next([]);
       this.hubConnection?.stop();
     }
   }
