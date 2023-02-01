67aa8cae64e1223ac653fcd42f8754c0b57e4a1f
Andrew Zakutko <zakutkoandrey@gmail.com>
2023-02-01 15:40:39 +0200

    Pre-deployment

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -17,17 +17,6 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => {
-        this.toastr.error(error.error);
-        console.log(error);
-      },
-      complete: () => console.log('Request has completed') 
-    });
-  }
-
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
